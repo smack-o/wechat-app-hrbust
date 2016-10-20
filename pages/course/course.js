188d72ff74dddf84ddd333098177a46abeee5241
smackgg <smackgg@gmail.com>
2016-10-20 19:10:57 +0800

    update

--- pages/course/course.js
+++ pages/course/course.js
@@ -8,21 +8,37 @@ Page({
   },
   onLoad: function() {
     console.log('course onload');
-    const username = wx.getStorageSync('username');
-    const password = wx.getStorageSync('password');
-    console.log(username,password,22222);
-    wx.request({
-      url: 'http://test.gebilaowu.cn/test',
-      data: {
-        username,
-        password,
-      },
-      header: {
-        'Content-Type': 'application/json'
-      },
-      success: function(res) {
-        console.log(res.data)
-      }
-    });
+    const that = this;
+    const courseData = wx.getStorageSync('courseData');
+
+    if (courseData) {
+      that.setData({
+        courseData,
+      });
+    } else {
+      const username = wx.getStorageSync('username');
+      const password = wx.getStorageSync('password');
+      wx.request({
+        url: 'http://test.gebilaowu.cn/test',
+        data: {
+          username,
+          password,
+        },
+        header: {
+          'Content-Type': 'application/json'
+        },
+        success: function(res) {
+          // console.log(res.data)
+          wx.setStorage({
+            key: 'courseData',
+            value: res.data
+          });
+          that.setData({
+            courseData: res.data
+          });
+        }
+      });
+    }
+    // wx.setStorage();
   }
 })

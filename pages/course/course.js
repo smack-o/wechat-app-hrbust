d892c1bffad0d701c74a957fd846321e2e9b3bba
smackgg <smackgg@gmail.com>
2016-10-23 20:55:48 +0800

    new

--- pages/course/course.js
+++ pages/course/course.js
@@ -1,10 +1,46 @@
 //index.js
 //获取应用实例
 Page({
+  data: {
+    timeStyle: 'timeLeftRight',
+    isTouch: false,
+  },
   gotoCourse: function(e) {
     wx.redirectTo({
       url: '../course/course'
     })
+  },
+  touchendPage: function(e) {
+    console.log(1);
+    // this.setData({
+    //   isTouch: false,
+    //   timeStyle: 'timeLeftRight'
+    // });
+  },
+  touchmovePage: function(e) {
+    console.log(e);
+    // const touchmoveX = e.touches[0].pageX;
+    // const touchmoveY = e.touches[0].pageY;
+    // if (this.data.isTouch) {
+    //   return;
+    // }
+    // if ( Math.abs(touchmoveY - this.pageY) > Math.abs(touchmoveX - this.pageX) ) {
+    //   console.log("top-down");
+    //   this.setData({
+    //     timeStyle: 'timeTopDown',
+    //     isTouch: true
+    //   });
+    // } else {
+    //   console.log("left-right");
+    //   this.setData({
+    //     timeStyle: 'timeLeftRight',
+    //     isTouch: true
+    //   });
+    // }
+  },
+  touchstartPage: function(e) {
+    this.pageX = e.touches[0].pageX;
+    this.pageY = e.touches[0].pageY;
   },
   onLoad: function() {
     console.log('course onload');
@@ -31,7 +67,7 @@ Page({
           // console.log(res.data)
           wx.setStorage({
             key: 'courseData',
-            value: res.data
+            data: res.data
           });
           that.setData({
             courseData: res.data

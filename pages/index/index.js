1cabbdbada707b091d2b8f4e586ff82047698f4a
smackgg <smackgg@gmail.com>
2017-02-14 11:26:09 +0800

    first page is home not index

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,21 +3,24 @@
 var app = getApp()
 Page({
   loginButton: function(e) {
-    if (this.data.selectUsername) {
-      wx.redirectTo({
-        url: '../home/home'
-      })
-    } else {
-      wx.redirectTo({
-        url: '../login/login'
-      })
-    }
-  },
-  onLoad: function () {
-    console.log('onLoad')
-    const selectUsername = wx.getStorageSync('selectUsername');
-    this.setData({
-      selectUsername
+    wx.redirectTo({
+      url: '../home/home'
     })
-  }
+    // if (this.data.selectUsername) {
+    //   wx.redirectTo({
+    //     url: '../home/home'
+    //   })
+    // } else {
+    //   wx.redirectTo({
+    //     url: '../login/login'
+    //   })
+    // }
+  },
+  // onLoad: function () {
+  //   console.log('onLoad')
+  //   const selectUsername = wx.getStorageSync('selectUsername');
+  //   this.setData({
+  //     selectUsername
+  //   })
+  // }
 })

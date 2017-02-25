5308c3a1b91ccbab49f7d771e04474118c67864b
smackgg <smackgg@gmail.com>
2017-02-25 21:49:35 +0800

    update

--- src/utils/get-request-url.js
+++ src/utils/get-request-url.js
@@ -10,9 +10,9 @@ if (NODE_ENV === 'dev') {
 }
 
 // es5 版本
-// if('NODE_ENV' === 'dev') { // NODE_ENV 要写字符串
-//  server = server1;
-// } else if('NODE_ENV' === 'production') {
-//  server = server2;
+// if ('NODE_ENV' === 'dev') {
+//     url = dev;
+// } else if ('NODE_ENV' === 'production') {
+//     url = production;
 // }
 module.exports = url;

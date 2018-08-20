801b42d2c4e2dd53c0922a95b54457125fe7b80a
smackgg <smackgg@gmail.com>
2018-08-20 15:56:08 +0800

    init wepay

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,29 @@
 module.exports = {
-	"extends": "airbnb-base",
-	"parser": "babel-eslint",
-	"globals" : {
-		"Page": true,
-		"App": true,
-		"wx" : true,
-		"getApp":true,
-		"NODE_ENV":true,
-	},
-	"plugins": [
-		'html'
-	],
-	"rules" : {
-		// "global-require": 0,
-		"indent": [0, "tab"], // 去掉tab约定,IDE会有问题
-		"new-cap": ["error", { "capIsNew": false }],
-		"no-console": [2, { "allow": [ "warn", "error" ]}],
-		"no-unused-expressions": ["error", { "allowShortCircuit": true }],
-		"radix": ["error", "as-needed"]
-	},
-};
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: 'standard',
+  // required to lint *.wpy files
+  plugins: [
+    'html'
+  ],
+  settings: {
+    'html/html-extensions': ['.html', '.wpy']
+  },
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0
+  }
+}

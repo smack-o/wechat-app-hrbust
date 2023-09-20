868661e0b34e61bcdfd96296ea8426b5ab874555
smackgg <smackgg@gmail.com>
2023-09-20 16:57:31 +0800

    feat: 移除资源相关逻辑；修改上课时间

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { Dispatch, bindActionCreators } from 'redux'
 import Taro from '@tarojs/taro'

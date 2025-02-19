10883cab4a0225bb343d4cd8ec1ac3e8d9307e6b
smackgg <smackgg@gmail.com>
2025-02-19 17:31:07 +0800

    feat: remove dist

--- packages/taro-client/dist/comp.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-(wx["webpackJsonp"] = wx["webpackJsonp"] || []).push([["comp"],{},
-/******/ function(__webpack_require__) { // webpackRuntimeModules
-/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
-/******/ __webpack_require__.O(0, ["taro","common"], function() { return __webpack_exec__("../../node_modules/.pnpm/@tarojs+webpack5-runner@3.6.13_@babel+core@7.26.9_@swc+core@1.3.23_@types+react@18.3.18_5e6abc700bd088d52e17986f24db757a/node_modules/@tarojs/webpack5-runner/dist/template/comp.js"); });
-/******/ var __webpack_exports__ = __webpack_require__.O();
-/******/ }
-]);
